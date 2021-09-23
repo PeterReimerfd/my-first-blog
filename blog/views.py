3cb405db0d5d8e3d4bca8deee9b385c3ddbaf978
preimer <Peter_Reimer@fd.org>
2021-09-23 17:49:21 -0500

    maybe this time it'll let text appear?

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.shortcuts import render, HttpResponse
 from .models import Post
 from django.utils import timezone
+import json
 
 # Create your views here.
 
+def index(request):
+    print("hello")
+    return(HttpResponse('something'))
+
 def post_list(request):
     posts = Post.objects.all().order_by('published_date')
+    print(posts.first().text)
     return render(request,'blog/post_list.html',{'posts': posts})
+
+#def post_list(request):
+ #   posts = Post.objects.all().order_by('published_date')
+ #   print(posts)
+ #   return render(request,'blog/post_list.html',{'posts': posts})
