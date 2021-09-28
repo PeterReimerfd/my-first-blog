ba6aed0d6f5f8e49f655fa6dd54cc4f37e3a1ee6
preimer <Peter_Reimer@fd.org>
2021-09-28 15:12:17 -0500

    Added views etc

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import Post
 from django.utils import timezone
 import json
@@ -14,6 +14,10 @@ def post_list(request):
     posts = Post.objects.all().order_by('published_date')
     print(posts.first().text)
     return render(request,'blog/post_list.html',{'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(request,'blog/post_detail.html',{'post': post})
 
 #def post_list(request):
  #   posts = Post.objects.all().order_by('published_date')
